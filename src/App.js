7d21ec3d786d4b1cfb89596bf2046c132d4d9f46
agungd3v <mailtotuyul1@gmail.com>
2021-07-01 18:25:24 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View, SafeAreaView, ScrollView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Its Work</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
