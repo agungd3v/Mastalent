9016a5bbbed368b51ebfdb93799b78ca6e5d951c
agungd3v <mailtotuyul1@gmail.com>
2021-07-01 19:28:49 +0700

    add global style and custom fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View, SafeAreaView, ScrollView} from 'react-native';
+import {style} from './helpers/styles';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView contentContainerStyle={{flex: 1}}>
+        <View style={style.container}>
           <Text>Its Work</Text>
         </View>
       </ScrollView>
